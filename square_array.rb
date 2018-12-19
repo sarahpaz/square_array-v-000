9b1e6f1661a1e44be2f4bf7b4cd0a4952f6086e1
Sarah Paz <sarah.paz@gmail.com>
2018-12-19 13:43:30 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,6 @@
-# def square_array(array)
-#   numbers = []
-#
-#   array.each {|number| numbers << number ** 2}
-#   numbers
-# end
-
 def square_array(array)
-  array.collect {|number| numbers << number ** 2}
-  numbers
+  squared = []
+
+  array.each {|number| squared << number ** 2}
+  squared
 end
